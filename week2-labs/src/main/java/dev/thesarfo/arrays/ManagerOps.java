1a56ca6b22f91a83c2ca3eacc6a24401b680a45a
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-10-29 09:44:59 +0000

    week1

--- week2-labs/src/main/java/dev/thesarfo/arrays/ManagerOps.java
+++
@@ -1,9 +0,0 @@
-package dev.thesarfo.arrays;
-
-public interface ManagerOps {
-
-    void addStudent(String name);
-    void searchStudent(String name);
-    void deleteStudent(String name);
-    void displayStudents();
-}
